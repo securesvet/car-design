da20beb94aabdaa74ef47c1fa4cdbe4d1f9424d8
Svet <mursvyats@gmail.com>
2023-10-19 20:51:56 +0500

    refactoring js code

--- js/app.js
+++ js/app.js
@@ -3,14 +3,26 @@ console.log(colorOptions)
 
 
 // Implementing selecting color option 
-let selectedElement; 
+console.log(colorOptions)
+let selectedElementByDefault = colorOptions[0];
+console.log(colorOptions);
+
+function selectElement(element) {
+    element.classList.add('selected-option');
+}
+
+function deselectElement(element) {
+    element.classList.remove('selected-option');
+}
+
+let selectedElement = selectedElementByDefault;
+
 colorOptions.forEach(element => {
     element.addEventListener('click', function() {
         if (selectedElement) {
-            selectedElement.style.outline = element.style.outline;
+            deselectElement(selectedElement);
         }
+        selectElement(element);
         selectedElement = element;
-        selectedElement.style.transition = "outline 100ms ease";
-        selectedElement.style.outline = "solid 3px blue";
     })
 });
