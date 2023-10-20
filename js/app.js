d8dd04e1f7e62fa3092823605b116fad691c77f6
Svet <mursvyats@gmail.com>
2023-10-20 19:25:54 +0500

    Added description when button is pressed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 const colorOptions = document.querySelectorAll('.circle');
 const carImage = document.querySelector('.car-image')
-
-console.log()
+const colorDescriptionDisplay = document.querySelector('.color-description');
+console.log(colorDescriptionDisplay)
 // Implementing selecting color option 
 let selectedElementByDefault = colorOptions[0];
+
+let colorDescriptions = {
+    'black': 'Space Black',
+    'white': 'Shiny White',
+    'blue': 'Blueish blue'
+}
 
 function selectElement(element) {
     element.classList.add('selected-option');
@@ -23,6 +29,8 @@ colorOptions.forEach(element => {
             selectElement(element);
             carImage.style.transition = "opacity 500ms ease";
             carImage.style.opacity = "0%";
+            colorDescriptionDisplay.innerHTML = `<b>${colorDescriptions[element.classList[0]]}</b>`;
+
             carImage.addEventListener('webkitTransitionEnd', function() { 
                 carImage.src = stringForCarImagePath.concat(element.classList[0], '.png');
                 carImage.style.opacity = "100%";
