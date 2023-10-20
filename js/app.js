c2e05eaf7cc5b492086d14c2dfcb518af8228b3c
Svet <mursvyats@gmail.com>
2023-10-20 23:11:24 +0500

    Modal window popup added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const colorOptions = document.querySelectorAll('.circle');
 const carImage = document.querySelector('.car-image')
 const colorDescriptionDisplay = document.querySelector('.color-description');
-console.log(colorDescriptionDisplay)
+
+
 // Implementing selecting color option 
 let selectedElementByDefault = colorOptions[0];
 
@@ -23,28 +24,30 @@ function deselectElement(element) {
     element.classList.remove('selected-option');
 }
 
-// Default setup
+// Start Default setup
 let selectedElement = selectedElementByDefault;
 
 setColorDescription(selectedElement);
+
+// End of Default setup
 
 let indexToColor = {0: 'black', 1: 'white', 2: 'blue'};
 const stringForCarImagePath = 'img/cars/car_';
 colorOptions.forEach(element => {
     element.addEventListener('click', function() {
-        if (selectedElement != element) {
-            deselectElement(selectedElement);
-            selectElement(element);
-            carImage.style.transition = "opacity 500ms ease";
-            carImage.style.opacity = "0%";
-            setColorDescription(element);
+        if (selectedElement === element) return;
+        deselectElement(selectedElement);
+        selectElement(element);
+        carImage.style.transition = "opacity 500ms ease";
+        carImage.style.opacity = "0%";
+        setColorDescription(element);
 
-            carImage.addEventListener('webkitTransitionEnd', function() { 
-                carImage.src = stringForCarImagePath.concat(element.classList[0], '.png');
-                carImage.style.opacity = "100%";
-           }, false );
-            
-            selectedElement = element;
-        }
+        carImage.addEventListener('webkitTransitionEnd', function() { 
+            carImage.src = stringForCarImagePath.concat(element.classList[0], '.png');
+            carImage.style.opacity = "100%";
+
+        }, false );
+
+        selectedElement = element;
     })
 });
